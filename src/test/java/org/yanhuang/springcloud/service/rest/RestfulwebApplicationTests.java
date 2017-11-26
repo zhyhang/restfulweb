215fd6534b4cc454ba07e69085aa390af5ee11e2
zhyhang <zhyhang1978@gmail.com>
2017-11-26 22:55:40 +0800

    let test example pass

--- src/test/java/org/yanhuang/springcloud/service/rest/RestfulwebApplicationTests.java
+++ src/test/java/org/yanhuang/springcloud/service/rest/RestfulwebApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.yanhuang.springcloud.rest.RestfulwebApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes= {RestfulwebApplication.class})
 public class RestfulwebApplicationTests {
 
 	@Test
