33b9a61da9fafa14216afe99db62ef1f8b28b88c
zhyhang <zhyhang1978@gmail.com>
2017-12-19 23:29:58 +0800

    add roles field

--- src/main/java/org/yanhuang/springcloud/rest/jpa/domain/security/Privilege.java
+++ src/main/java/org/yanhuang/springcloud/rest/jpa/domain/security/Privilege.java
@@ -1,10 +1,17 @@
 package org.yanhuang.springcloud.rest.jpa.domain.security;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import org.yanhuang.springcloud.rest.entity.BaseEntity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="security_privilege")
@@ -33,4 +40,7 @@ public class Privilege extends BaseEntity{
 	@Column(name="type")
 	private privilegeType type;
 	
+	@ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY,mappedBy="privileges")
+	@JsonIgnore
+	private Set<Role> roles;
 }
